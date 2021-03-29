2fd96642d7f300c293f1f77523ea4362b2fa6c3d
liuzehao <liuzehao139@163.com>
2021-03-29 21:27:52 +0800

    1

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -87,7 +87,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		if err != nil {
 			return err
 		}
-		//todo:update
+
 		if arg.FromAccountID < arg.ToAccountID {
 			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 		} else {
